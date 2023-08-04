350c7ceda14239e9c02e2fe33929b20144dd85af
Matt Owen <MATTOWEN88@OUTLOOK.COM>
2023-08-04 21:56:58 +0100

    03082023

--- code/cutscene.py
+++ code/cutscene.py
@@ -1,4 +1,4 @@
-import pygame
+
 from state import State
 from settings import *
 from dialogue import Dialogue
@@ -14,15 +14,18 @@ class Cutscene0(State):
 		self.bar_height = 0
 		self.target_height = HEIGHT * 0.1
 		self.blackbar = pygame.Surface((WIDTH, self.bar_height))
+
 		self.target = pygame.math.Vector2(self.zone.target.rect.center)
 		self.new_pos = pygame.math.Vector2()
 
 		self.timer = 0
+		self.int_time = 0
 
 		self.dialogue_active = False
 
 	def create_dialogue(self, target_sprite, number):
-		self.dialogue = Dialogue(self.game, self.zone, target_sprite, number)
+		
+		self.dialogue = Dialogue(self.game, self.zone, target_sprite, number).enter_state()
 		self.dialogue_active = True
 
 	def move_camera(self):
@@ -42,47 +45,43 @@ class Cutscene0(State):
 		elif self.bar_height < self.target_height - 1:  
 		    self.bar_height += (self.target_height - self.bar_height) / 60
 
+
 		pygame.draw.rect(screen, BLACK, (0, 0, WIDTH, self.bar_height))
 		pygame.draw.rect(screen, BLACK, (0, HEIGHT - self.bar_height, WIDTH, self.target_height))
 
 	def sequence(self):
 
-		if self.timer < 60:
+		if self.int_time < 60:
 			self.target = pygame.math.Vector2(self.zone.target.rect.center)
 
 		# move the camera new position after short cooldown above
-		elif self.timer < 120:
-			self.create_dialogue(self.zone.target, self.number)
+		elif self.int_time == 180:
+			Dialogue(self.game, self.zone, self.zone.npc, self.number, 60).enter_state()
+		# elif 239 < self.timer < 241:
+		# 	self.dialogue_active = False
+		elif self.int_time < 300:
 			self.new_pos = pygame.math.Vector2(self.zone.npc.rect.center)
-		elif self.timer < 180:
-			self.new_pos = pygame.math.Vector2(self.zone.target.rect.center)
-		elif self.timer < 240:
-			self.dialogue_active = False
-			self.new_pos = pygame.math.Vector2(RES)
-		elif self.timer < 300:
-			self.new_pos = pygame.math.Vector2(self.zone.npc.rect.center)
-
-		# set the camera back to player before exiting cutscene
-		elif self.timer < 360:
+		elif self.int_time == 420:
+		 	Dialogue(self.game, self.zone, self.zone.target, self.number, 120).enter_state()
+		elif self.int_time < 480:
 			self.new_pos = pygame.math.Vector2(self.zone.target.rect.center)
 
-		elif self.timer > 420:
+		elif self.int_time > 540:
 			self.opening = False
-
 
 	def update(self, dt):
 		self.game.reset_keys()
 		self.timer += dt
+		self.int_time = int(self.timer)
 		self.prev_state.update(dt)
-
 
 	def render(self, screen):
 		self.move_camera()
-		self.prev_state.rendered_sprites.offset_draw(self.target)
 		self.sequence()
-		if self.dialogue_active:
-			self.dialogue.render(screen)
+		self.prev_state.rendered_sprites.offset_draw(self.target)
+
 		self.blackbars(screen)
+
 
 class Cutscene1(Cutscene0):
 
--- code/dialogue.py
+++ code/dialogue.py
@@ -1,34 +1,79 @@
-
+from state import State
 from settings import *
 
-class Dialogue():
-	def __init__(self, game, zone, sprite, cutscene_number):
+class Dialogue(State):
+	def __init__(self, game, zone, sprite, cutscene_number, duration):
+		State.__init__(self, game)
 
+		self.game = game
 		self.sprite = sprite
 		self.cutscene_number = cutscene_number
+		self.duration = duration
 		self.offset = self.sprite.zone.rendered_sprites.offset
 
 		self.dialogue_dict = DIALOGUE[self.cutscene_number]
 		
-		self.active = True
+		self.opening = True
 
-		print(self.dialogue_dict)
+		self.box_width = 0
+		self.center = (self.sprite.rect.centerx - self.offset.x, self.sprite.rect.top - 25 - self.offset.y)
+		self.target_width = WIDTH * 0.5
+		self.box = pygame.Surface((WIDTH, self.box_width))
 
-	def stop(self):
-		self.game.reset_keys()
-		self.exit_state()
+
+		self.current_char_index = 0
+		self.current_dialogue = DIALOGUE[self.cutscene_number]
+
+		self.timer = 0
+
+
+	def opening_box(self, screen):
+		if not self.opening:
+		    self.box_width -= (self.target_width - self.box_width) / 10
+
+		    if self.box_width <= 0:
+		        self.box_width = 0
+		        self.opening = True
+		        self.exit_state()
+
+		elif self.box_width < self.target_width - 1:  
+		    self.box_width += (self.target_width - self.box_width) / 10
+
+		if self.timer > self.duration:
+			self.opening = False
+
+		pygame.draw.rect(screen, BLACK, (self.center[0] - self.box_width/2, self.center[1] - 30, self.box_width, 60))
+
+	def draw_text(self):
+		text_to_display = self.current_dialogue[:self.current_char_index]  # Get characters up to current_char_index
+		self.game.render_text(text_to_display, WHITE, self.game.small_font, self.center)
 
 	def update(self, dt):
+		self.timer += dt
 
 		if ACTIONS['return']: 
 			self.exit_state()
 		self.game.reset_keys()
 
-		# self.prev_state.update(dt)
+		self.prev_state.update(dt)
 
-		# if not self.active:
-		# 	self.exit_state()
+		if self.timer > 5:
+			self.timer = 0
+			self.current_char_index += 1
+			if self.current_char_index > len(self.current_dialogue):
+				self.current_char_index = len(self.current_dialogue)
+
+		print(self.timer)
 
 	def render(self, screen):
-		# self.prev_state.render(screen)
-		pygame.draw.rect(screen, PINK, (self.sprite.rect.left - 75 - self.offset.x, self.sprite.rect.top - 75 - self.offset.y, 100, 50))
+		self.prev_state.render(screen)
+		rect = pygame.Rect(self.sprite.rect.left - 75 - self.offset.x, self.sprite.rect.top - 55 - self.offset.y, 100, 50)
+
+		self.center = (self.sprite.rect.centerx - self.offset.x, self.sprite.rect.top - 35 - self.offset.y)
+		#pygame.draw.rect(screen, LIGHT_GREEN, rect, border_radius=5)
+		self.opening_box(screen)
+
+		# if self.box_width >= self.target_width -1:
+		# 	self.game.render_text(self.dialogue_dict, WHITE, self.game.small_font, self.center)
+
+		self.draw_text()	
